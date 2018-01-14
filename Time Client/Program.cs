75916446e12a4ec9ebb586390fe2c1ad56c3205a
Christian Rhodes <christian.rhodes@oit.edu>
2018-01-14 02:18:20 -0800

    first commit

---
+++ Time Client/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Time_Client
+{
+    class Program
+    {
+        //================= PRIVATE MEMBER =================//
+        private static Socket clientSocket = new Socket
+            (AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+
+        //================= MAIN PROGRAM =================//
+        static void Main(string[] args)
+        {
+            Console.Title = "Client";
+            ConnectToServer();
+            SendData();
+        }
+        //**********************************************//
+
+
+        //============== LOOP CONNECT METHOD ================//
+        private static void ConnectToServer()
+        {
+            int attempts = 0;
+
+            while (!clientSocket.Connected)
+            {
+                try
+                {
+                    attempts++;
+                    Console.WriteLine("Connection Attempts: " + attempts.ToString());
+                    clientSocket.Connect(IPAddress.Loopback, 100);
+                }
+                
+                catch (SocketException)
+                {
+                    Console.Clear();
+                }
+            }
+
+            Console.Clear();
+            Console.WriteLine("Connected");
+
+        }
+        //*************************************************//
+
+        //============== LOOP CONNECT METHOD ================//
+        private static void SendData()
+        {
+            while (true)
+            {
+                Console.Write("Enter a request: ");
+                Console.WriteLine("Time Request Input(s): ");
+                Console.WriteLine("get time | get euro time | get japan time | get gmt time | get nz time");
+                string request = Console.ReadLine();
+
+                byte[] buffer = Encoding.ASCII.GetBytes(request);
+                clientSocket.Send(buffer, 0, buffer.Length, SocketFlags.None);
+
+                byte[] receivedBuffer = new byte[1024];
+                int receipt = clientSocket.Receive(receivedBuffer, SocketFlags.None);
+                if (receipt == 0) return;
+                byte[] data = new byte[receipt];
+                Array.Copy(receivedBuffer, data, receipt);
+                string text = Encoding.ASCII.GetString(data);
+                Console.WriteLine("Received: " + text);
+            }
+        }
+
+
+
+    }
+}
---
+++ TimeServer/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace TimeServer
+{
+    class Program
+    {
+        //==================== PRIVATE MEMBERS ======================//
+
+        private static byte[] buffer = new byte[1024]; // global buffer of 1024 bytes.
+        private static List<Socket> clientSockets = new List<Socket>();
+        private static Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+
+        static void Main(string[] args)
+        {
+            Console.Title = "World Time Server";
+            SetupServer();
+            Console.ReadLine();
+        }
+
+        private static void SetupServer()
+        {
+            Console.WriteLine("Setting up Time Server...");
+            serverSocket.Bind(new IPEndPoint(IPAddress.Any, 100));
+            serverSocket.Listen(5);
+            //Listening to connection...
+            serverSocket.BeginAccept(new AsyncCallback(AcceptCallBack), null);
+        }
+
+        private static void AcceptCallBack(IAsyncResult AR)
+        {
+            // this method will accept incoming connection attempt and create a new socket to handle remote host connection
+            Socket socket = serverSocket.EndAccept(AR); 
+            clientSockets.Add(socket);  // adding new socket (defined in the ReceiveCallBack method) to our client list
+            Console.WriteLine("Time Client Connected");
+            socket.BeginReceive(buffer, 0, buffer.Length, SocketFlags.None, new AsyncCallback(ReceiveCallBack), socket);
+            serverSocket.BeginAccept(new AsyncCallback(AcceptCallBack), null); // accept again. without this it will no longer accept any more connection.
+        }
+
+        private static void ReceiveCallBack(IAsyncResult AR)
+        {
+            Socket socket = (Socket)AR.AsyncState;
+            // determine how many data we are receiving
+            int received = socket.EndReceive(AR);
+            //temp buffer
+            byte[] dataBuf = new byte[received];
+            Array.Copy(buffer, dataBuf, received);
+            string text = Encoding.ASCII.GetString(dataBuf);
+            Console.WriteLine("Text Received: " + text);
+
+            string response = string.Empty;
+
+            if (text.ToLower() == "get time")
+            {
+                response = DateTime.Now.ToLongTimeString();
+
+            }
+
+            else if (text.ToLower() == "get euro time")
+            {
+                var remoteTimeZone = TimeZoneInfo.FindSystemTimeZoneById("Central European Standard Time");
+                var remoteTime = TimeZoneInfo.ConvertTime(DateTime.Now, remoteTimeZone);
+                response = remoteTime.TimeOfDay.ToString();
+            }
+
+            else if (text.ToLower() == "get japan time")
+            {
+                var remoteTimeZone = TimeZoneInfo.FindSystemTimeZoneById("Tokyo Standard Time");
+                var remoteTime = TimeZoneInfo.ConvertTime(DateTime.Now, remoteTimeZone);
+                response = remoteTime.TimeOfDay.ToString();
+            }
+
+            else if (text.ToLower() == "get gmt time")
+            {
+                var remoteTimeZone = TimeZoneInfo.FindSystemTimeZoneById("GMT Standard Time");
+                var remoteTime = TimeZoneInfo.ConvertTime(DateTime.Now, remoteTimeZone);
+                response = remoteTime.TimeOfDay.ToString();
+            }
+
+            else if (text.ToLower() == "get nz time")
+            {
+                var remoteTimeZone = TimeZoneInfo.FindSystemTimeZoneById("New Zealand Standard Time");
+                var remoteTime = TimeZoneInfo.ConvertTime(DateTime.Now, remoteTimeZone);
+                response = remoteTime.TimeOfDay.ToString();
+            }
+
+
+            else
+            {
+                response = "invalid request";
+                
+            }
+
+            byte[] data = Encoding.ASCII.GetBytes(response);
+            socket.BeginSend(data, 0, data.Length, SocketFlags.None, new AsyncCallback(SendCallBack), socket);
+            socket.BeginReceive(buffer, 0, buffer.Length, SocketFlags.None, new AsyncCallback(ReceiveCallBack), socket);
+        }
+
+        private static void SendCallBack(IAsyncResult AR)
+        {
+            Socket socket = (Socket)AR.AsyncState;
+            socket.EndSend(AR);
+        }
+    }
+}
